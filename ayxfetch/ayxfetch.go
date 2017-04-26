d708d98682969f916b3632259ca24d88a3c5be96
JP Kabler <john.kabler@gmail.com>
2017-04-26 11:43:39 -0600

    additional changes and refactoring, added functionality for fetching data connections

--- ayxfetch/ayxfetch.go
+++ ayxfetch/ayxfetch.go
@@ -6,13 +6,105 @@ import (
 	"goryx/ayxauth"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+
+	"github.com/gocarina/gocsv"
 )
+
+// FetchConnections pulls back data from the serverdataconnections
+// and returns them in a ConnectionsList struct.
+func FetchConnections(ayxSigner *ayxauth.AyxSigner, connectionType string) *ConnectionsList {
+	signer := *ayxSigner
+	var endpoint string
+	switch connectionType {
+	case `server`:
+		endpoint = `serverdataconnections/`
+	case `system`:
+		endpoint = `systemdataconnections/`
+	}
+
+	httpMethod := `GET`
+
+	requestURL := signer.BuildRequest(endpoint, httpMethod)
+	// Send the request
+	resp, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	var connections ConnectionsList
+
+	err = json.NewDecoder(resp.Body).Decode(&connections)
+
+	if err != nil {
+		log.Println("ERROR:", err)
+	}
+	fmt.Println(connections)
+	fmt.Println(resp.Status)
+	return &connections
+}
+
+// WriteConnections method takes the JSON returned from
+// Connections APIs and writes to CSV
+func WriteConnections(connectionsList ConnectionsList, records *[]*ConnectionRecord, outputPath string) {
+	if len(connectionsList) > 0 {
+		for _, conn := range connectionsList {
+			*records = append(*records, &ConnectionRecord{
+				ConnectionID:      conn.ConnectionID,
+				ConnectionName:    conn.ConnectionName,
+				ConnectionString:  conn.ConnectionString,
+				ConnectionType:    conn.ConnectionType,
+				SubscriptionCount: conn.SubscriptionCount,
+				UserCount:         conn.UserCount})
+		}
+		csvContent, csvErr := gocsv.MarshalString(records)
+		if csvErr != nil {
+			panic(csvErr)
+		}
+		fmt.Println(csvContent)
+
+		outFile, fileErr := os.Create(outputPath)
+		if fileErr != nil {
+			panic(fileErr)
+		}
+
+		writeErr := gocsv.MarshalFile(records, outFile)
+		if writeErr != nil {
+			panic(writeErr)
+		}
+	}
+}
+
+// ConnectionsList is a struct that holds the list
+// of data connections returned by the server and/or
+// system data connections API.
+type ConnectionsList []struct {
+	ConnectionID      string `json:"connectionId"`
+	ConnectionName    string `json:"connectionName"`
+	ConnectionString  string `json:"connectionString"`
+	ConnectionType    string `json:"connectionType"`
+	SubscriptionCount int    `json:"subscriptionCount"`
+	UserCount         int    `json:"userCount"`
+}
+
+// ConnectionRecord is a struct used to hold records
+// for writing the connections info to CSV
+type ConnectionRecord struct {
+	ConnectionID      string `csv:"ConnectionID"`
+	ConnectionName    string `csv:"ConnectionName"`
+	ConnectionString  string `csv:"ConnectionString"`
+	ConnectionType    string `csv:"ConnectionType"`
+	SubscriptionCount int    `csv:"SubscriptionCount"`
+	UserCount         int    `csv:"UserCount"`
+}
 
 // FetchWorkflows hits the workflows endpoint of
 // the Gallery API and returns a list of the workflows
 // present on the server.
-func FetchWorkflows(ayxSigner *ayxauth.AyxSigner) {
+func FetchWorkflows(ayxSigner *ayxauth.AyxSigner) *WorkflowList {
 	signer := *ayxSigner
 	var endpoint string
 	if strings.Contains(signer.GalleryURL, `/admin/v1`) {
@@ -46,10 +138,56 @@ func FetchWorkflows(ayxSigner *ayxauth.AyxSigner) {
 
 	if err != nil {
 		log.Println("ERROR:", err)
-		return
 	}
 	fmt.Println(workflows)
 	fmt.Println(resp.Status)
+	return &workflows
+}
+
+// WriteWorkflows function will iterate through the workflows
+// response and write each record out to a slice
+// of pointers to records (note that it's a pointer which will
+// allow for modifying the slice in place.)
+func WriteWorkflows(workflowList WorkflowList, records *[]*WorkflowRecord, outputPath string) {
+	if len(workflowList) > 0 {
+		for _, workflow := range workflowList {
+			var collectionNames []string
+			for _, collection := range workflow.Collections {
+				collectionNames = append(collectionNames, collection.CollectionName)
+			}
+			collectionList := strings.Join(collectionNames, ",")
+			*records = append(*records, &WorkflowRecord{
+				FileName:       workflow.FileName,
+				ID:             workflow.ID,
+				Author:         workflow.MetaInfo.Author,
+				Description:    workflow.MetaInfo.Description,
+				Name:           workflow.MetaInfo.Name,
+				PackageType:    workflow.PackageType,
+				Public:         workflow.Public,
+				RunCount:       workflow.RunCount,
+				RunDisabled:    workflow.RunDisabled,
+				SubscriptionID: workflow.SubscriptionID,
+				UploadDate:     workflow.UploadDate,
+				Collections:    collectionList,
+				Version:        strconv.Itoa(workflow.PublishedVersionNumber)})
+		}
+
+		csvContent, csvErr := gocsv.MarshalString(records)
+		if csvErr != nil {
+			panic(csvErr)
+		}
+		fmt.Println(csvContent)
+
+		outFile, fileErr := os.Create(outputPath)
+		if fileErr != nil {
+			panic(fileErr)
+		}
+
+		writeErr := gocsv.MarshalFile(records, outFile)
+		if writeErr != nil {
+			panic(writeErr)
+		}
+	}
 }
 
 //WorkflowList is a struct that holds the list of workflows
@@ -92,4 +230,22 @@ type WorkflowList []struct {
 		SubscriptionID string      `json:"subscriptionId"`
 	} `json:"publishedVersionOwner"`
 	SubscriptionName string `json:"subscriptionName"`
+}
+
+// WorkflowRecord struct is used for each row
+// written out to the CSV file.
+type WorkflowRecord struct {
+	FileName       string `csv:"FileName"`
+	ID             string `csv:"ID"`
+	Author         string `csv:"Author"`
+	Description    string `csv:"Description"`
+	Name           string `csv:"Name"`
+	PackageType    int    `csv:"PackageType"`
+	Public         bool   `csv:"Public"`
+	RunCount       int    `csv:"runCount"`
+	RunDisabled    bool   `csv:"runDisabled"`
+	SubscriptionID string `csv:"subscriptionId"`
+	UploadDate     string `csv:"uploadDate"`
+	Version        string `csv:"version"`
+	Collections    string `csv:"collections"`
 }
--- goryx.go
+++ goryx.go
@@ -10,46 +10,44 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Printf("usage: %s <alteryx_consumerKey> <alteryx_clientKey> <galleryUrl>\n",
+		fmt.Printf(`usage: %s <alteryx_consumerKey> <alteryx_clientKey> <galleryUrl>
+			<endpoint> <file_output_path>
+
+			possible values for "endpoint" parameter:
+
+			workflows :  Fetches list of all workflows.
+			connections : Fetches list of all data connections (both server and system)`,
 			filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
 	var consumerKey = os.Args[1]
 	var consumerSecret = os.Args[2]
 	var galleryURL = os.Args[3]
+	var outputPath = os.Args[5]
+	var endpoint = os.Args[4]
 
 	signer := ayxauth.AyxSigner{
 		ConsumerKey:    consumerKey,
 		ConsumerSecret: consumerSecret,
 		GalleryURL:     galleryURL}
 
-	ayxfetch.FetchWorkflows(&signer)
+	switch endpoint {
+	case "workflows":
+		fmt.Println("workflows")
+		workflowList := ayxfetch.FetchWorkflows(&signer)
+		fmt.Println(workflowList)
+		output := []*ayxfetch.WorkflowRecord{}
+		ayxfetch.WriteWorkflows(*workflowList, &output, outputPath)
 
-	// requestURL := signer.BuildRequest(endpoint, httpMethod)
-	//
-	// resp, err := http.Get(requestURL)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// defer resp.Body.Close()
-	//
-	// fmt.Println(resp)
-	// fmt.Println(resp.Body)
-	//
-	// var workflows WorkflowList
-	//
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	//
-	// err = json.NewDecoder(resp.Body).Decode(&workflows)
-	// //err = json.Unmarshal(stringResponse, &workflows)
-	// if err != nil {
-	// 	log.Println("ERROR:", err)
-	// 	return
-	// }
-	// fmt.Println(workflows)
-	// fmt.Println(resp.Status)
-
+	case "connections":
+		fmt.Println("connections")
+		connectionsList := ayxfetch.FetchConnections(&signer, "server")
+		systemConnectionsList := ayxfetch.FetchConnections(&signer, "system")
+		for _, conn := range *systemConnectionsList {
+			*connectionsList = append(*connectionsList, conn)
+		}
+		fmt.Println(connectionsList)
+		output := []*ayxfetch.ConnectionRecord{}
+		ayxfetch.WriteConnections(*connectionsList, &output, outputPath)
+	}
 }
